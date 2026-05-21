fe026d679e5a0bc9f572a41cee92372d88706cc4
Mercz0 <geovannimercza@gmail.com>
2026-05-20 19:53:07 -0600

    feat: add percentage operation logic and unit tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Http\Controllers\OperationController;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,5 +13,17 @@ class ExampleTest extends TestCase
     public function test_that_true_is_true(): void
     {
         $this->assertTrue(true);
+    }
+
+    public function test_percentage_result(): void
+    {
+        $controller = new OperationController;
+
+        $result = $controller->calculatePercentage(150, 20);
+
+        $this->assertIsFloat($result);
+        $this->assertNotNull($result);
+        $this->assertEquals(30, $result);
+        $this->assertGreaterThan(0, $result);
     }
 }
