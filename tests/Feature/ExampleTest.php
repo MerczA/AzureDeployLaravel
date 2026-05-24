c7601d2f369e2eebd88595d8dd2ad86599790e6f
Rafachad007 <rafelnrvz@gmail.com>
2026-05-23 23:49:13 -0600

    test: agregar pruebas par impar

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,4 +16,24 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_numero_par(): void
+    {
+        $response = $this->post('/par-impar', [
+            'numero' => 8,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSee('El número es par');
+    }
+
+    public function test_numero_impar(): void
+    {
+        $response = $this->post('/par-impar', [
+            'numero' => 7,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSee('El número es impar');
+    }
 }
